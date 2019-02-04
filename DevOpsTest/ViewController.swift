8116d9de49e518ab320c8766feb7b8af28693742
Ömer Burak Kır <omerburak.kir@mobven.com>
2019-02-04 09:44:53 +0300

    Project files

---
+++ DevOpsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevOpsTest
+//
+//  Created by Ömer Burak Kır on 4.02.2019.
+//  Copyright © 2019 Ömer Burak Kır. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
